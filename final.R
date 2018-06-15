b757e40355fef79e667c6e4e5b98e644285e94ef
BaiiYuan <b05902002@ntu.edu.tw>
2018-06-15 10:22:50 +0800

    r

--- final.R
+++ final.R
@@ -1,11 +1,20 @@
 df <- read.csv("responses.csv")
+df$Gender = as.integer(df$Gender == "female")
+#df$Education = as.factor(df$Education)
+#as.numeric
+#df$Education[df$Education == "secondary school"] = 2
+
 mode = "Smoking"
-df1 <- subset(df, select = c("Music", "Smoking", "Alcohol", "Keeping.promises",
+df1 <- subset(df, select = c("Music", "Gender", "Education", "Only.child",
+                             "Smoking", "Alcohol", "Keeping.promises",
                              "Reliability", "Cheating.in.school", "Lying", "Punctuality",
                              "Getting.angry", "Charity"))
 df2 <- subset(df, select = c("Music", "Finances", "Branded.clothing", "Entertainment.spending",
                              "Spending.on.looks", "Spending.on.gadgets"))
 
-if (mode == "Smoking") {
-  
-}
+
+
+
+
+a = cor.test(df1$Music, df1$Keeping.promises, method = 'pearson')
+
