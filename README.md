0d46851ccc35c258747f9414f43d198a4e10f0b1
BaiiYuan <b05902002@ntu.edu.tw>
2018-06-15 13:58:19 +0800

    r

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Psych Statistics Final
 Final of Psych Statistics with dataset: Young People Survey
+
+
+### Smoke
+> Correlations : -0.08877139
+
+### Alcohol
+> Correlations : -0.1192905
+
+
+### Keeping promises
+
+t = 3.0508, df = 961, p-value = 0.002345
+
+> Correlations : 0.09793982 
+
+
+### Reliability
+
+t = 0.97867, df = 961, p-value = 0.328
+
+> Correlations : 0.03155413 
+
+
+### Cheating in school
+
+t = -2.9152, df = 961, p-value = 0.003638
+
+> Correlations : -0.09362422 
+
+### Lying
+
+t = -1.5098, df = 961, p-value = 0.1314
+
+> Correlations : -0.04864677 
+
+### Punctuality
+
+t = 1.4693, df = 961, p-value = 0.1421
+
+> Correlations : 0.04734226 
+
+### Getting.angry
+
+t = -1.8536, df = 961, p-value = 0.0641
+
+> Correlations : -0.05968761 
+
+### Charity
+
+t = 4.7051, df = 961, p-value = 2.909e-06
+
+> Correlations : 0.1500589 
--- final.R
+++ final.R
@@ -1,20 +1,71 @@
-df <- read.csv("responses.csv")
-df$Gender = as.integer(df$Gender == "female")
-#df$Education = as.factor(df$Education)
-#as.numeric
-#df$Education[df$Education == "secondary school"] = 2
+df <- read.csv("pre-processed_data.csv")
+df <- na.omit(df)
+#df$Gender = as.integer(df$Gender == "female")
 
-mode = "Smoking"
-df1 <- subset(df, select = c("Music", "Gender", "Education", "Only.child",
+
+#mode = "Smoking"
+df1 <- subset(df, select = c("Musical.instruments",
+                             #"Gender", "Education", "Only.child",
                              "Smoking", "Alcohol", "Keeping.promises",
                              "Reliability", "Cheating.in.school", "Lying", "Punctuality",
                              "Getting.angry", "Charity"))
-df2 <- subset(df, select = c("Music", "Finances", "Branded.clothing", "Entertainment.spending",
-                             "Spending.on.looks", "Spending.on.gadgets"))
+#df1 <- na.omit(df1)
+#summary(df1)
+
+
+install.packages("polycor")
+library("polycor")
+
+# Smoke
+df_smoke <- subset(df, select = c("Musical.instruments", "Smoking"))
+df_smoke <- df_smoke[(df_smoke$Smoking) != 2,]
+df_smoke$Smoking = as.integer(df_smoke$Smoking >= 2)
+#polychoric(df_smoke[,c(1,2)])
+polychor(df_smoke$Musical.instruments, df_smoke$Smoking)
+
+
+# Alcohol
+df_alc <- subset(df, select = c("Musical.instruments", "Alcohol"))
+df_alc$Alcohol = as.integer(df_alc$Alcohol >= 1)
+#polychoric(df_alc[,c(1,2)])
+polychor(df_alc$Musical.instruments, df_alc$Alcohol)
+
+# Keeping promises
+cor.test(rank(df1$Musical.instruments), rank(df1$Keeping.promises), method = 'pearson')
+# Reliability
+cor.test(rank(df1$Musical.instruments), rank(df1$Reliability), method = 'pearson')
+# Cheating in school
+cor.test(rank(df1$Musical.instruments), rank(df1$Cheating.in.school), method = 'pearson')
+# Lying
+cor.test(rank(df1$Musical.instruments), rank(df1$Lying), method = 'pearson')
+# Punctuality
+cor.test(rank(df1$Musical.instruments), rank(df1$Punctuality), method = 'pearson')
+# Getting.angry
+cor.test(rank(df1$Musical.instruments), rank(df1$Getting.angry), method = 'pearson')
+# Charity
+cor.test(rank(df1$Musical.instruments), rank(df1$Charity), method = 'pearson')
 
 
 
+# not used
+
+df2 <- subset(df, select = c("Musical.instruments", "Finances", "Branded.clothing","Entertainment.spending", 
+                             "Spending.on.looks", "Spending.on.gadgets")) #1010
+df2 <- na.omit(df2) #998
 
 
-a = cor.test(df1$Music, df1$Keeping.promises, method = 'pearson')
+df2$Musical.instruments <- rank(df2$Musical.instruments)
+df2$Finances <- rank(df2$Finances)
+df2$Branded.clothing <- rank(df2$Branded.clothing)
+df2$Entertainment.spending <- rank(df2$Entertainment.spending)
+df2$Spending.on.looks <- rank(df2$Spending.on.looks)
+df2$Spending.on.gadgets <- rank(df2$Spending.on.gadgets)
+
+cor.test(df2$Musical.instruments, df2$Finances, method = 'pearson')
+cor.test(df2$Musical.instruments, df2$Branded.clothing, method = 'pearson')
+cor.test(df2$Musical.instruments, df2$Entertainment.spending, method = 'pearson')
+cor.test(df2$Musical.instruments, df2$Spending.on.looks, method = 'pearson')
+cor.test(df2$Musical.instruments, df2$Spending.on.gadgets, method = 'pearson')
+
+
 
